065d033908315a2a7769fbd354afa41d1d627002
Kevin Laptop <kcortesrodrigue@itba.edu.ar>
2017-05-20 16:43:47 -0300

    Ejemplo 2 con Nacho para probar ahora

--- Java implementation/src/Main.java
+++ Java implementation/src/Main.java
@@ -16,33 +16,37 @@ public class Main {
 		List<Ult> ary0 = new LinkedList<>();
 		List<Ult> ary1 = new LinkedList<>();
 		List<Ult> ary2 = new LinkedList<>();
+		List<Ult> ary3 = new LinkedList<>();
 		
-		int[] ar1 = {3,3,5};
+		int[] ar1 = {2,2,3};
 		Ult u1 = new Ult(0, "1ULT", ar1, "1KLT", "1P");
 
 		int[] ar2 = {1,1,2};
 		Ult u2 = new Ult(0, "2ULT", ar2, "1KLT", "1P");
 		
-		ary0.add(u1);
-		ary0.add(u2);
-		
 		int[] ar3 = {2,1,3,1,1};
 		Ult u3 = new Ult(1, "3ULT", ar3, "2KLT", "1P");
 		
-		int[] ar4 = {3,2,4,1,1};
-		Ult u4 = new Ult(1, "4ULT", ar4, "2KLT", "1P");
-		
-		ary1.add(u3);
-		ary1.add(u4);
+		int[] ar4 = {1,2,1,2,1};
+		Ult u4 = new Ult(3, "4ULT", ar4, "2KLT", "1P");
 		
 		int[] ar5 = {1,1,3};
 		Ult u5 = new Ult(2, "5ULT", ar5, "3KLT", "2P");
 		
+		int[] ar6 = {2,1,2};
+		Ult u6 = new Ult(1, "6ULT", ar6, "4KLT", "3P");
+		
+		ary0.add(u1);
+		ary0.add(u2);
+		ary1.add(u3);
+		ary1.add(u6);
 		ary2.add(u5);
+		ary3.add(u4);
 		
 		readyMap.put(0, ary0);
 		readyMap.put(1, ary1);
-		readyMap.put(2, ary2);	
+		readyMap.put(2, ary2);
+		readyMap.put(2, ary3);
 		
 		// Second create all Resources; cpu, io, etc
 		Core core = new Core(1);
